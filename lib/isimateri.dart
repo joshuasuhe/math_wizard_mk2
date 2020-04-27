8a5d2de0d1920792c41bfcfc8914cfa45a113a09
avinash322 <avinashawin48@gmail.com>
2020-04-27 21:45:03 +0700

    materi page

--- lib/isimateri.dart
+++ lib/isimateri.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';import 'package:math_wizard_mk2/login.dart';
-import 'package:math_wizard_mk2/materi.dart';
-import 'package:math_wizard_mk2/signup.dart';
-import 'package:math_wizard_mk2/ranking.dart';
-import 'package:math_wizard_mk2/profile.dart';
-import 'package:math_wizard_mk2/utilities/constants.dart';
-import 'package:math_wizard_mk2/category.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter/services.dart';
 
 class halamanmateri extends StatefulWidget {
   @override
@@ -23,6 +13,7 @@ class _halamanmateriState extends State<halamanmateri> {
         [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
     return Scaffold(
       body: CustomScrollView(
+
         slivers: <Widget>[
           SliverAppBar(
             leading: new IconButton(
@@ -34,7 +25,7 @@ class _halamanmateriState extends State<halamanmateri> {
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               title: Text(
-                "Materi Pelajaran",
+                "Materi Perhitungan Bilangan Bulat ",
                 style:
                     TextStyle(color: Colors.black, fontFamily: "Poppins-Bold"),
               ),
@@ -43,43 +34,80 @@ class _halamanmateriState extends State<halamanmateri> {
             ),
           ),
           SliverFillRemaining(
-            child: new Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/img/body.png'),
-                      fit: BoxFit.cover)),
-              child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: new SingleChildScrollView(
-                        child: new Text(
-                          '''1 Description that is too long in text format(data diambil dari firebase)  ''' +
-                              '''2 Description that is too long in text format(data diambil dari firebase)  ''' +
-                              '''3 Description that is too long in text format(data diambil dari firebase)  ''' +
-                              ''' 4 Description that is too long in text format(data diambil dari firebase) ''' +
-                              '''5 Description that is too long in text format(data diambil dari firebase)  ''' +
-                              '''6 Description that is too long in text format(data diambil dari firebase)  ''' +
-                              '''7 Description that is too long in text format(data diambil dari firebase)  ''' +
-                              '''8 Description that is too long in text format(data diambil dari firebase) ''' +
-                              '''9 Description that is too long in text format(data diambil dari firebase) ''' +
-                              '''10 Description that is too long in text format(data diambil dari firebase)''',
-                          style: new TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            child: new Column(
+              children: <Widget>[GestureDetector
+              (child:Hero(
+                  tag: 'capture1',
+                  child: Image.network(
+                      'https://firebasestorage.googleapis.com/v0/b/tes1-baa07.appspot.com/o/Materi%2FKelas%206%2FBAB1%20Perhitungan%20Bilangan%20Bulat%2FCapture1.PNG?alt=media&token=fbce3743-efb2-4cd7-a8f3-145666d97656'),
+                ),
+                onTap:(){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return DetailScreen1();
+                  }));
+
+
+                }),
+                GestureDetector(
+                  child:Hero(
+                  tag: 'capture2',
+                  child: Image.network(
+                      'https://firebasestorage.googleapis.com/v0/b/tes1-baa07.appspot.com/o/Materi%2FKelas%206%2FBAB1%20Perhitungan%20Bilangan%20Bulat%2FCapture2.PNG?alt=media&token=e27b5985-1775-49f5-8e74-48c3052a111e')
+                ),   onTap:(){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return DetailScreen2();
+                  }));
+                
+                
+                })
+                
+              ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class DetailScreen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'capture1',
+            child: Image.network(
+                      'https://firebasestorage.googleapis.com/v0/b/tes1-baa07.appspot.com/o/Materi%2FKelas%206%2FBAB1%20Perhitungan%20Bilangan%20Bulat%2FCapture1.PNG?alt=media&token=fbce3743-efb2-4cd7-a8f3-145666d97656',
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
+
+
+class DetailScreen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'capture2',
+            child: Image.network(
+                      'https://firebasestorage.googleapis.com/v0/b/tes1-baa07.appspot.com/o/Materi%2FKelas%206%2FBAB1%20Perhitungan%20Bilangan%20Bulat%2FCapture2.PNG?alt=media&token=e27b5985-1775-49f5-8e74-48c3052a111e'
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
